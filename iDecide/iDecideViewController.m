4a3d8289e3411c44cab00f4d104fb82ef0179672
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-01 22:44:47 -0400

    Add list of strings for response

--- iDecide/iDecideViewController.m
+++ iDecide/iDecideViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "iDecideViewController.h"
+#import <stdlib.h>
 
 @interface iDecideViewController ()
 
@@ -28,6 +29,19 @@
 
 - (IBAction)makeDecision:(id)sender
 {
+    NSArray *responses = @[@"It is certain", @"It is decidedly so", @"Without a doubt",
+                           @"Yes definitely", @"You may rely on it", @"As I see it yes",
+                           @"Most likely", @"Outlook good", @"Yes", @"Signs point to yes",
+                           @"Reply hazy try again", @"Ask again later", @"Better not tell you now",
+                           @"Cannot predict now", @"Concentrate and ask again", @"Don't count on it",
+                           @"My reply is no", @"My sources say no", @"Outlook not so good",
+                           @"Very doubtful"];
+
+    NSString *response = [responses objectAtIndex:arc4random_uniform(responses.count)];
+
+
+    NSLog(@"Response is: %@", response);
+
     NSLog(@"Now computing decision from various factors");
 }
 
